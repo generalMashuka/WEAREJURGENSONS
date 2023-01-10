a2255c1756b5b5740af80af424b62e5bffba5481
yanakovas <yanakovas1203@gmail.com>
2023-01-10 09:59:24 +0300

    test

--- frontend/src/features/items/selectors.ts
+++ frontend/src/features/items/selectors.ts
@@ -1,8 +1,9 @@
-import { RootState } from "../../store";
-import CartItem from "../cart/types/CartItem";
-import Item from "./types/Item";
+import { RootState } from '../../store';
+import CartItem from '../cart/types/CartItem';
+import Item from './types/Item';
 
 export const selectItems = (state: RootState): Item[] => state.items.items;
 export const selectLoaderror = (state: RootState): string | undefined =>
   state.items.loadError;
-export const selectCartItems = (state: RootState): CartItem[] => state.cart.cart;
+export const selectCartItems = (state: RootState): CartItem[] =>
+  state.cart.cart;
